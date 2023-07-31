1dff122fc3a52ebcb0bc1d94906532f4051f743a
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-01 01:46:40 +0600

    bookmarked blogs

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Blog.css'
+const Blog = () => {
+    return (
+        <div className='blog'>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minima?</span>
+        </div>
+    );
+};
+
+export default Blog;
--- src/components/Saved/Saved.js
+++ src/components/Saved/Saved.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Saved.css';
+import Blog from '../Blog/Blog';
 const Saved = () => {
     return (
         <div className='saved-container'>
-            hello
+            <div className='time'>Spent Time on read: 0 min</div>
+
+            <div className='blogs'>
+                <h3>Bookmarked Blogs: 0</h3>
+                <Blog></Blog>
+                <Blog></Blog>
+                <Blog></Blog>
+                <Blog></Blog>
+            </div>
         </div>
     );
 };
